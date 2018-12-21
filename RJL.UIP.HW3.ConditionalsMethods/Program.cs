6f370b8580dfbb4771297ebd25284e2ae538953d
Yurusalova <Yurusalova@gmail.com>
2018-12-21 16:13:07 +0200

    added HW3

---
+++ RJL.UIP.HW3.ConditionalsMethods/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RJL.UIP.HW3.ConditionalsMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
